a1aeff21b951ef4e62c3acf0ca0feb592809e62c
Adam Pahlevi <adam.pahlevi@gmail.com>
2017-02-25 06:44:34 +0700

    front end in slim + bootstrap4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'order/index'
+
+  get 'order/new'
+
+  get 'order/create'
+
+  get 'order/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
