f1b36bb573bfd663d75c199cacadef3419341234
Adam Pahlevi <adam.pahlevi@gmail.com>
2017-02-25 07:02:25 +0700

    foundational templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  get 'order/index'
-
-  get 'order/new'
-
-  get 'order/create'
-
-  get 'order/show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :orders, only: [:index, :new, :create, :show]
 end
