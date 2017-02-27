7cd308e2b3ee0a8f8db9e2e0e8861059bae244ba
Adam Pahlevi <adam.pahlevi@gmail.com>
2017-02-27 07:58:12 +0700

    add comment to nameable

--- db/migrate/20170224104532_create_properies.rb
+++ db/migrate/20170224104532_create_properies.rb
@@ -3,6 +3,7 @@ class CreateProperies < ActiveRecord::Migration[5.0]
     create_table :properties do |t|
       t.string :name, null: false
       t.string :desc, null: false
+      # when creating the item, the property value will be embedded into the name
       t.boolean :nameable, null: false, default: true
 
       t.timestamps
