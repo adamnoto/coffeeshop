1e539a27123dd8b4a47a6075ccbfb8b6d3c92002
Adam Pahlevi <adam.pahlevi@gmail.com>
2017-02-27 08:05:57 +0700

    redirect to order new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index
+    redirect_to new_order_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'application#index'
   resources :orders, only: [:index, :new, :create, :show]
   resources :items, only: [:index]
   resources :item_properties, only: [:index]
